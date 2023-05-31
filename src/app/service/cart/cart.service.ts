107080436c92165d9a684185d4169f1a04ae870a
Dimitrios Chalatsoglou <chalatsoglou-dimitrios@web.de>
2023-05-31 19:28:32 +0200

    Service und Model für warenkorb(Cart) hinzugefügt

---
+++ src/app/service/cart/cart.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CartDTO } from '../../model/cart/cartDTO';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CartService {
+  constructor(private httpClient: HttpClient) {}
+
+  public listCarts(): Observable<CartDTO[]> {
+    console.log('execute listProducts');
+    return this.httpClient.get<CartDTO[]>('/warenkorb');
+  }
+}
