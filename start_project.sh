c3aa9215b6e5eb7bbdcd0196d112625fc249ad47
Dimitrios Chalatsoglou <chalatsoglou-dimitrios@web.de>
2023-07-02 20:54:51 +0200

    Skript für MAC/Linux

---
+++ start_project.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Skript zum Starten des gesamten Projekts für MacOS und Linux
+
+# Navigiere zum Homeveerzeichnis
+cd ~
+
+# Erstelle Vereichnis für das ganze Projekt
+mkdir Fullstack-Projekt
+
+# Navigiere zum Projektordner
+cd Fullstack-Projekt
+
+# Backend-Repository clonen
+git clone "https://github.com/yungsnowx/fullstackprojekt-backend.git"
+
+# Frontend-Repository clonen
+git clone "https://github.com/yungsnowx/fullstackprojekt-frontend.git"
+
+# Starte die Datenbank
+bash fullstackprojekt-backend/start_database_for_mac.sh
+
+# Backend starten
+cd fullstackprojekt-backend && npm run dev
+
+# Frontend starten
+cd ../fullstackprojekt-frontend && npm start -o
